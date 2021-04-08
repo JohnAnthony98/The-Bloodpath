228c570eaf971c5010fc2b41f3616426615f81aa
JohnAnthony98 <janthony10@mail.csuchico.edu>
2021-04-08 16:28:32 -0700

    Implemented moving ground enemy from side to side

---
+++ The Bloodpath/Assets/Scripts/EnemyScripts/GroundEnemy.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundEnemy : MonoBehaviour
+{
+    private bool findingRightBound;
+    private bool findingLeftBound;
+    private bool foundGround;
+    private Vector3 rightBound;
+    private Vector3 leftBound;
+    private float travelTime;
+    private float startTime;
+
+    private GameObject moveCheckerPreFab;
+    private GameObject moveChecker;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foundGround = false;
+        findingRightBound = false;
+        findingLeftBound = false;
+        moveCheckerPreFab = Resources.Load("PreFabs/Enemies/GroundEnemyMoveChecker") as GameObject;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if(foundGround == false)
+        {
+            return;
+        }
+
+        if (findingRightBound)
+        {
+            //move right
+            Vector3 newPos = transform.position;
+            newPos.x += 0.2f;
+            transform.position = newPos;
+        }
+        else if (findingLeftBound)
+        {
+            //move left
+            Vector3 newPos = transform.position;
+            newPos.x -= 0.2f;
+            transform.position = newPos;
+            startTime = Time.time;
+        }
+        else
+        {
+            //move between two bounds
+            float u = (Mathf.Sin(Time.time - startTime - (Mathf.PI/2)) + 1)  / 2;
+            
+
+
+            Vector3 newPos = (1 - u) * leftBound + u * rightBound;
+            this.gameObject.transform.position = newPos;
+        }
+    }
+
+    public void SetBound()
+    {
+        if (findingRightBound)
+        {
+            rightBound = transform.position;
+            findingRightBound = false;
+            findingLeftBound = true;
+        }
+        else if (findingLeftBound)
+        {
+            leftBound = transform.position;
+            findingLeftBound = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            if(foundGround == false)
+            {
+                foundGround = true;
+                findingRightBound = true;
+                moveChecker = Instantiate(moveCheckerPreFab);
+                moveChecker.GetComponent<GroundEnemyMoveChecker>().SetEnemy(this.gameObject);
+                return;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PlayerAttack"))
+        {
+            Destroy(this);
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            player.GetComponent<PlayerTesting>().ResetMoves();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ The Bloodpath/Assets/Scripts/EnemyScripts/GroundEnemyMoveChecker.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundEnemyMoveChecker : MonoBehaviour
+{
+    private GameObject attatchedEnemy = null;
+    private bool scaleSet;
+    private bool findingRightBound;
+    private bool findingLeftBound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        findingRightBound = true;
+        findingLeftBound = false;
+        scaleSet = false;
+        if(attatchedEnemy != null)
+        {
+            this.gameObject.transform.localScale = attatchedEnemy.transform.localScale;
+            scaleSet = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(attatchedEnemy == null)
+        {
+            return;
+        }
+        if (scaleSet == false)
+        {
+            this.gameObject.transform.localScale = attatchedEnemy.transform.localScale;
+            scaleSet = true;
+        }
+
+        if (findingRightBound)
+        {
+            Vector3 newPos = attatchedEnemy.transform.position;
+            newPos.x += transform.localScale.x;
+            transform.position = newPos;
+        }
+        else if (findingLeftBound)
+        {
+            Vector3 newPos = attatchedEnemy.transform.position;
+            newPos.x -= transform.localScale.x;
+            transform.position = newPos;
+        }
+    }
+
+    public void SetEnemy(GameObject enemy)
+    {
+        attatchedEnemy = enemy;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            attatchedEnemy.GetComponent<GroundEnemy>().SetBound();
+            if (findingRightBound)
+            {
+                findingRightBound = false;
+                findingLeftBound = true;
+            }
+            else
+            {
+                Destroy(this);
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(this.gameObject);
+    }
+}
