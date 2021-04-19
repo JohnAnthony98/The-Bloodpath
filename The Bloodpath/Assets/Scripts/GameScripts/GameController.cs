199018426182eb6c36dc545099492c6c5df2221b
RedThunder5227 <ezioisabadass@gmail.com>
2021-04-19 16:23:19 -0700

    Updated Win Screen
    win screen now has a next level button and a main menu button, allow multiple levels

--- The Bloodpath/Assets/Scripts/GameScripts/GameController.cs
+++ The Bloodpath/Assets/Scripts/GameScripts/GameController.cs
@@ -117,6 +117,11 @@ public class GameController : MonoBehaviour
         }
     }
 
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
     public void QuitGame()
     {
         //Debug.Log("Quit Button Pressed");
