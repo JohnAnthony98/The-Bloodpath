1b0df8b9fc7c50fc25f94af328e683181d0cb795
RedThunder5227 <ezioisabadass@gmail.com>
2021-05-04 09:40:41 -0700

    visual checkpoint added

---
+++ The Bloodpath/Assets/Scripts/checkpoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkpoint : MonoBehaviour
+{
+    private Renderer cubeRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubeRenderer = gameObject.transform.Find("indicator").GetComponent<Renderer>();
+        cubeRenderer.material.color = Color.black;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            cubeRenderer.material.color = Color.green;
+        }
+    }
+}
